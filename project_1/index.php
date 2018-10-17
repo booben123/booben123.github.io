5b7aa55f8c95e9ffac539ad351bf64cec4463e72
ArtemGit <booben123@gmail.com>
2018-10-17 12:26:28 +0300

    nice site

---
+++ project_1/index.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="../sass/style.sass">
+	<link rel="stylesheet" href="animate/animate.css">
+</head>
+<body>
+
+<div class="container">
+	<div class="top-menu ">
+		<img src="img/forest.png" alt="Логотип">
+		<ul class="top-nav">
+			<li><a href="">Главная</a></li>
+			<li><a href="">Контакты</a>
+				<ul>
+          			<li><a href="#">Адрес</a></li>
+          			<li><a href="#">Телефон</a></li>
+          			<li><a href="#">Email</a></li>
+        		</ul>
+        	</li>	
+			<li><a href="">Вопрос-ответ</a></li>
+			<li><a href="">Наши услуги</a></li>
+			<li><a href="">Продукция</a></li>
+		</ul>
+	</div>
+	<div class="row">
+		<div class="sidebar-block-top animated flipInX col-sm-3">
+			<ul class="sidebar-nav">
+				<li><a href="">Ламинат</a> </li>
+				<li><a href="">Фанера</a></li>
+				<li><a href="">Двп</a></li>
+				<li><a href="">Дсп</a></li>
+				<li><a href="">УЗБ</a></li>
+			</ul>
+		</div>	
+		<div class="sidebar block1-bottom">
+			<ul>
+				<li>НИЗЗЗЗЗЗ</li>
+				<li>и это тоже</li>
+				<li>и это тоже</li>
+				<li>и это тоже</li>
+				<li>и это тоже</li>
+			</ul>
+		</div>
+		<div class="content col-sm-9">
+			<h1>Контент</h1>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi doloremque voluptatem quaerat nulla eligendi, esse, obcaecati voluptatibus rem vitae alias ullam, suscipit quod dolor accusamus reprehenderit et animi totam voluptate.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit facilis aut in, labore inventore! Porro at molestiae illum delectus eos, veritatis totam quod esse, soluta aliquid temporibus sequi asperiores! Aliquam?</div>
+			<input type="submit">
+		</div>
+	</div>
+</div>
+<div class="row background">
+	<div class="footer ">
+		<ul class="footer-bar-left col-sm-6">
+			<li>Дуб</li>
+			<li>фанера</li>
+			<li>УСБ</li>
+			<li>ДВП</li>
+			<li>ДСП</li>
+		</ul>
+		<ul class="footer-bar-right col-sm-6">
+			<li><a href="">Главная</a></li>
+			<li><a href="">Контакты</a></li>
+			<li><a href="">Вопрос-ответ</a></li>
+			<li><a href="">Наши услуги</a></li>
+			<li><a href="">Продукция</a></li>
+		</ul>
+	</div>
+</div>
+
+</body>
+</html>
